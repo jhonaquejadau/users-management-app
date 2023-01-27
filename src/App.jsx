3d32930259c49aa8855be142385f83147a9ce278
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-27 17:31:03 -0500

    react-router-dom configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import { Auth } from "./pages/auth/Auth";
+import { Routes, Route } from "react-router-dom";
+import { AuthSignIn } from "./pages/auth/AuthSignIn";
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
+    <div className="w-full h-screen flex justify-center items-center">
+      <Routes>
+        <Route exact path="/" element={<Auth/>}/>
+        <Route path="/signin" element={<AuthSignIn/>}/>
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
