1155885c25187a7c7449e0d4692e40d6a7ab3735
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-27 17:58:49 -0500

    Form validation implemented

--- src/pages/components/auth/FormInputs.jsx
+++ src/pages/components/auth/FormInputs.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
-export const FormInputs = ({label, errorMessage, onChange, ...inputs}) => {
+export const FormInputs = ({ label, errorMessage, onChange, ...inputs }) => {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocused = (e) => {
+    e.preventDefault();
+    setFocused(true);
+  };
+
   return (
     <div className="flex flex-col my-2 w-full h-full py-1">
       <label className="mb-2 capitalize">{label}</label>
-      <input className="border-2 p-2" {...inputs} onChange={onChange} />
-      <span>{errorMessage ? errorMessage : ''}</span>
+      <input className="border-[1px] focus:outline-none p-2" {...inputs} onChange={onChange} onBlur={handleFocused} focused={focused.toString()}/>
+      <span>{errorMessage}</span>
     </div>
   );
 };
--- src/utilities/signInInputs.js
+++ src/utilities/signInInputs.js
@@ -5,6 +5,10 @@ export const signInInputs = [
     type: "text",
     placeholder: "name",
     label: "name",
+    errorMessage:
+      "Username should be 3-16 characters and shouldn't include any special character!",
+    pattern: "^[A-Za-z0-9]{3,20}$",
+    required: true,
   },
   {
     id: 2,
@@ -12,6 +16,9 @@ export const signInInputs = [
     type: "text",
     placeholder: "lastname",
     label: "lastname",
+    errorMessage:
+      "Lastname should be 3-16 characters and shouldn't include any special character!",
+    required: true,
   },
   {
     id: 3,
@@ -19,6 +26,8 @@ export const signInInputs = [
     type: "text",
     placeholder: "email",
     label: "email",
+    errorMessage: "It should be a valid email address!",
+    required: true,
   },
   {
     id: 4,
@@ -26,12 +35,9 @@ export const signInInputs = [
     type: "password",
     placeholder: "password",
     label: "password",
-  },
-  {
-    id: 5,
-    name: "confirmPassword",
-    type: "password",
-    placeholder: "confirm password",
-    label: "confirm password",
-  },
+    errorMessage:
+      "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+    pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+    required: true,
+  }
 ];
