3d549988172bbd8f69397c9108b8da519aabdcb3
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-29 15:17:10 -0500

    code refactor

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/users-management">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
